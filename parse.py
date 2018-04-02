169f4bffd8031dc930a8a14b99f69a93e75b59db
OverLord2829 <37993717+OverLord2829@users.noreply.github.com>
2018-04-02 22:32:16 +0530

    Add files via upload

---
+++ parse.py
@@ -0,0 +1,30 @@
+import os
+import sqlite3
+import operator
+from collections import OrderedDict
+from urllib.parse import urlparse
+
+def parse(url):
+    try:
+        parse_url=urlparse(url)
+        url_query=parse_url.query
+        return url_query
+    except IndexError:
+        print("URL format error")
+
+data_path = os.path.expanduser('~')+"\AppData\Local\Google\Chrome\User Data\Default"
+files = os.listdir(data_path)
+
+history_db = os.path.join(data_path,'history')
+c = sqlite3.connect(history_db)
+cursor = c.cursor()
+select_statement = "SELECT urls.url from urls"
+cursor.execute(select_statement)
+results = cursor.fetchall()
+qeury_list = {}
+for url in reults:
+    x = parse(url)
+    if x not in querylist:
+        
+    
+        
