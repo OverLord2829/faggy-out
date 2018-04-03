5b4a4b6232167639c81ed8dfdc6c6ce0b7861bb8
OverLord2829 <37993717+OverLord2829@users.noreply.github.com>
2018-04-03 12:24:18 +0530

    Add files via upload

--- parse.py
+++ parse.py
@@ -1,36 +1,31 @@
 import os
 import sqlite3
-import operator
-from collections import OrderedDict
 from urllib.parse import urlparse
+
 
 def parse(url):
     try:
-        parse_url=urlparse(url)
-        url_query=parse_url.query
+        parse_url = urlparse(url)
+        url_query = parse_url.query
         return url_query
     except IndexError:
         print("URL format error")
 
-data_path = os.path.expanduser('~')+"\AppData\Local\Google\Chrome\User Data\Default"
+
+data_path: str = "{0}\\AppData\\Local\\Google\\Chrome\\User Data\\Default".format(os.path.expanduser('~'))
 files = os.listdir(data_path)
 
-history_db = os.path.join(data_path,'history')
+history_db = os.path.join(data_path, 'history')
 c = sqlite3.connect(history_db)
 cursor = c.cursor()
 select_statement = "SELECT urls.url from urls"
 cursor.execute(select_statement)
 results = cursor.fetchall()
 query_list = []
-for url in reults:
+for url in results:
     x = parse(url)
     if x is '':
         continue
     elif x not in query_list:
         query_list.insert(x)
-print (query_list)
-
-        
-        
-    
-        
+print(query_list)
