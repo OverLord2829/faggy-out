d8d2018f08c3ca95757584673ac8b05c299fff1f
OverLord2829 <37993717+OverLord2829@users.noreply.github.com>
2018-04-04 19:10:21 +0530

    Add files via upload

--- parse.py
+++ parse.py
@@ -1,15 +1,16 @@
 import os
 import sqlite3
 from urllib.parse import urlparse
+#import operator
+#from nltk.tokenize import sent_tokenize
 
-
-def parse(url):
-    try:
-        parse_url = urlparse(url)
-        url_query = parse_url.query
-        return url_query
-    except IndexError:
-        print("URL format error")
+#def parse(url):
+    #try:
+        #parse_url = urlparse(url)
+        #url_query = parse_url.query
+        #return url_query
+    #except IndexError:
+        #print("URL format error")
 
 
 data_path: str = "{0}\\AppData\\Local\\Google\\Chrome\\User Data\\Default".format(os.path.expanduser('~'))
@@ -18,14 +19,17 @@ files = os.listdir(data_path)
 history_db = os.path.join(data_path, 'history')
 c = sqlite3.connect(history_db)
 cursor = c.cursor()
-select_statement = "SELECT url from urls"
+select_statement = 'SELECT term from keyword_search_terms'
 cursor.execute(select_statement)
 results = cursor.fetchall()
-query_list = []
-for url in results:
-    x = parse(url)
-    if x is '':
-        continue
-    elif x not in query_list:
-        query_list.insert(x)
-print(query_list)
+print(results)
+#query_list = []
+#for urls in results:
+    #print(urls)
+    #x = urls
+    #print(urlparse('x'))
+    #if x is '':
+        #continue
+    #if x not in query_list:
+        #query_list.insert(x)
+#print(query_list)
